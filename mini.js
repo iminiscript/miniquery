1c1dc1d411a8b620a8a558e113567a29102fb58a
iminiscript <maninder@anattadesign.com>
2024-08-03 00:05:24 +0530

    Update ADD class function

--- mini.js
+++ mini.js
@@ -1,46 +1,56 @@
 (function(global) {
-  // MiniQuery function
-  function MiniQuery(selector) {
-      if (!(this instanceof MiniQuery)) {
-          return new MiniQuery(selector);
-      }
+    // MiniQuery function
+    function MiniQuery(selector) {
+        if (!(this instanceof MiniQuery)) {
+            return new MiniQuery(selector);
+        }
 
-      this.elements = document.querySelectorAll(selector);
-  }
+        if (typeof selector === 'string') {
+            this.elements = document.querySelectorAll(selector);
+        } else if (selector instanceof Element) {
+            this.elements = [selector];
+        } else if (selector instanceof NodeList) {
+            this.elements = selector;
+        } else {
+            this.elements = [];
+        }
+    }
 
-  // Add class method
-  MiniQuery.prototype.AC = function(className) {
-      this.elements.forEach(function(element) {
-          element.classList.add(className);
-      });
-      return this;
-  };
+    // Add class method
+    MiniQuery.prototype.AC = function(className) {
+        this.elements.forEach(function(element) {
+            element.classList.add(className);
+        });
+        return this;
+    };
 
-  // Remove class method
-  MiniQuery.prototype.RC = function(className) {
-      this.elements.forEach(function(element) {
-          element.classList.remove(className);
-      });
-      return this;
-  };
+    // Remove class method
+    MiniQuery.prototype.RC = function(className) {
+        this.elements.forEach(function(element) {
+            element.classList.remove(className);
+        });
+        return this;
+    };
 
-  // Toggle class method
-  MiniQuery.prototype.TC = function(className) {
-      this.elements.forEach(function(element) {
-          element.classList.toggle(className);
-      });
-      return this;
-  };
+    // Toggle class method
+    MiniQuery.prototype.TC = function(className) {
+        this.elements.forEach(function(element) {
+            element.classList.toggle(className);
+        });
+        return this;
+    };
 
-  // Click event method
-  MiniQuery.prototype.CLICK = function(handler) {
-      this.elements.forEach(function(element) {
-          element.addEventListener('click', handler);
-      });
-      return this;
-  };
+    // Click event method
+    MiniQuery.prototype.CLICK = function(handler) {
+        this.elements.forEach(function(element) {
+            element.addEventListener('click', function(event) {
+                handler.call(element, event);
+            });
+        });
+        return this;
+    };
 
-  // Expose MiniQuery to the global object
-  global.MQ = MiniQuery;
+    // Expose MiniQuery to the global object
+    global.MQ = MiniQuery;
 
 })(window);
